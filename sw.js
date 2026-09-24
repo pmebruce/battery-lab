48da577faf1ce6dd6602f0f914a45ff1e07efc0f
pmebruce <pmebruce@gmail.com>
2026-09-25 02:13:57 +0800

    Double battery silhouette outline thickness

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'battery-lab-github-v6';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/icon-192.png?v=6', './icons/icon-512.png?v=6', './apple-touch-icon.png?v=6'];
+const CACHE = 'battery-lab-github-v7';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/icon-192.png?v=7', './icons/icon-512.png?v=7', './apple-touch-icon.png?v=7'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', event => { if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return; event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))); });
